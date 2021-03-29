22e33976709753cb3b9090661f2146defa656007
abilashs003 <abilashs003@gmail.com>
2021-03-29 20:38:05 +0530

    updated arrays exercise

---
+++ exercise/arrays.js
@@ -0,0 +1,12 @@
+// declare an student array
+// It is an multi dimensional array
+// there will be a single student array
+// inside that student array you will get list of students array
+// [["name of a student", "class",[[maths,72], [science,86], [environment,65]],
+// ["name of a student", "class",[[maths,72], [science,86], [environment,65]]]
+
+// declare for 5 students then
+// try pop function for last element
+// try push also
+// unshift
+// shift
